a2e17bb5f3dba021307491ff32ef247dfc2dc484
Abhinandan <habhinandan98@gmail.com>
2018-10-18 21:03:12 +0530

    uploading image to mongodb database (pdf not complete)

---
+++ app/src/main/java/com/example/abhinandan/onlinedb/Backgroundworkerclasses/Backgroundworker_upload.java
@@ -0,0 +1,97 @@
+package com.example.abhinandan.onlinedb.Backgroundworkerclasses;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class Backgroundworker_upload extends AsyncTask<String,Void,String> {
+
+    Context context;
+    AlertDialog alertDialog;
+
+    public Backgroundworker_upload(Context ctx) {
+        this.context = ctx;
+    }
+
+    @Override
+    protected String doInBackground(String... voids) {
+        String type = voids[0];
+        String upload_url = "http://circularmanagement.000webhostapp.com/upload_mongo.php";
+        if (type.equals("upload")) {
+            try {
+                String userid = voids[1];
+                String image = voids[2];
+                String title = voids[3];
+                String semeseter = voids[4];
+                String branch = voids[5];
+                String section = voids[6];
+                URL url = new URL(upload_url);
+                HttpURLConnection htc = (HttpURLConnection) url.openConnection();
+                if (htc == null) {
+                    Toast.makeText(context, "connection failed", Toast.LENGTH_SHORT).show();
+                }
+                htc.setRequestMethod("POST");
+                htc.setDoOutput(true);
+                htc.setDoInput(true);
+                OutputStream os = htc.getOutputStream();
+                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+                String post_data = URLEncoder.encode("userid", "UTF-8") + "=" + URLEncoder.encode(userid, "UTF-8") + "&"
+                        + URLEncoder.encode("image", "UTF-8") + "=" + URLEncoder.encode(image, "UTF-8") + "&"
+                        + URLEncoder.encode("title", "UTF-8") + "=" + URLEncoder.encode(title, "UTF-8") + "&"
+                        + URLEncoder.encode("semester", "UTF-8") + "=" + URLEncoder.encode(semeseter, "UTF-8") + "&"
+                        + URLEncoder.encode("branch", "UTF-8") + "=" + URLEncoder.encode(branch, "UTF-8") + "&"
+                        + URLEncoder.encode("section", "UTF-8") + "=" + URLEncoder.encode(section, "UTF-8");
+                bw.write(post_data);
+                bw.flush();
+                bw.close();
+                os.close();
+                InputStream is = htc.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(is, "iso-8859-1"));
+                String result = "";
+                String line = "";
+                while ((line = br.readLine()) != null) {
+                    result += line;
+                }
+                br.close();
+                is.close();
+                htc.disconnect();
+                return result;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        alertDialog.setMessage("Upload Successful");
+        alertDialog.show();
+        super.onPostExecute(s);
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        alertDialog = new AlertDialog.Builder(context).create();
+        alertDialog.setTitle("Status");
+        alertDialog.setMessage("Uploading....");
+        alertDialog.show();
+    }
+}
--- app/src/main/java/com/example/abhinandan/onlinedb/fragments/add_circular.java
+++ app/src/main/java/com/example/abhinandan/onlinedb/fragments/add_circular.java
@@ -3,31 +3,45 @@ package com.example.abhinandan.onlinedb.fragments;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.abhinandan.onlinedb.Backgroundworkerclasses.Backgroundworker_upload;
 import com.example.abhinandan.onlinedb.R;
 import com.example.abhinandan.onlinedb.Teacherlayout;
+
+import java.io.ByteArrayOutputStream;
 
 import static android.app.Activity.RESULT_OK;
 
 
 public class add_circular extends android.support.v4.app.Fragment implements View.OnClickListener {
     ImageView imageView;
-    Button b1,b2;
+    EditText et;
+    String encodedimage;
+    SharedPreferences sp;
+    Button b1,b2,b3;
     TextView tv1;
+    Spinner s1,s2,s3;
     View views;
     int imagesel = 0;
     int pdfsel = 0;
@@ -43,9 +57,32 @@ public class add_circular extends android.support.v4.app.Fragment implements Vie
         tv1 = (TextView)views.findViewById(R.id.tvchoosepdf);
         b1 = (Button)views.findViewById(R.id.choosefile);
         b2 = (Button)views.findViewById(R.id.choosepdf);
+        b3 = (Button)views.findViewById(R.id.upload);
+        et = (EditText)views.findViewById(R.id.ettitle);
         b1.setOnClickListener(this);
         b2.setOnClickListener(this);
+        b3.setOnClickListener(this);
+        s1 = (Spinner)views.findViewById(R.id.chosen_sem);
+        s2 = (Spinner)views.findViewById(R.id.chosen_branch);
+        s3 = (Spinner)views.findViewById(R.id.chosen_section);
         context  = container.getContext();
+
+        ArrayAdapter<String> myada = new ArrayAdapter<String>(context,
+                android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.Semester));
+        myada.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s1.setAdapter(myada);
+
+        ArrayAdapter<String> myada1 = new ArrayAdapter<String>(context,
+                android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.Branch));
+        myada.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s2.setAdapter(myada1);
+
+        ArrayAdapter<String> myada2 = new ArrayAdapter<String>(context,
+                android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.Section));
+        myada.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s3.setAdapter(myada2);
+
+
         return views;
     }
 
@@ -57,6 +94,13 @@ public class add_circular extends android.support.v4.app.Fragment implements Vie
                 break;
             case R.id.choosepdf:
                 selectpdf();
+                break;
+            case R.id.upload:
+                Bitmap image = ((BitmapDrawable)imageView.getDrawable()).getBitmap();
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                image.compress(Bitmap.CompressFormat.JPEG,100,byteArrayOutputStream);
+                encodedimage = Base64.encodeToString(byteArrayOutputStream.toByteArray(),Base64.DEFAULT);
+                uploadthefile();
                 break;
         }
     }
@@ -101,5 +145,18 @@ public class add_circular extends android.support.v4.app.Fragment implements Vie
         intent.setType("application/pdf");
         intent.setAction(Intent.ACTION_GET_CONTENT);
         startActivityForResult(intent,READ_PDF);
+    }
+
+    public void uploadthefile(){
+        sp = context.getSharedPreferences("MY_SHARE",Context.MODE_PRIVATE);
+        String userid = sp.getString("UserID","");
+        String type = "upload";
+        String title = et.getText().toString();
+        String image = encodedimage;
+        String semester = s1.getSelectedItem().toString();
+        String branch = s2.getSelectedItem().toString();
+        String section = s3.getSelectedItem().toString();
+        Backgroundworker_upload bu = new Backgroundworker_upload(context);
+        bu.execute(type,userid,image,title,semester,branch,section);
     }
 }
