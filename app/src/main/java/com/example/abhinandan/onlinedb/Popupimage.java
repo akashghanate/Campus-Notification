3af85b882716e0eac93061c70c9fbb990ddc881a
Abhinandan <habhinandan98@gmail.com>
2018-11-03 15:50:07 +0530

    zooming in and out of images

--- app/src/main/java/com/example/abhinandan/onlinedb/Popupimage.java
+++ app/src/main/java/com/example/abhinandan/onlinedb/Popupimage.java
@@ -2,13 +2,28 @@ package com.example.abhinandan.onlinedb;
 
 import android.app.Activity;
 import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.graphics.PointF;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.DisplayMetrics;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageView;
 
-public class Popupimage extends Activity {
 
+public class Popupimage extends Activity implements View.OnTouchListener {
+
+    private static final float MIN_ZOOM = 1f,MAX_ZOOM = 1f;
+    Matrix matrix = new Matrix();
+    Matrix savedMatrix = new Matrix();
+    static final int NONE = 0;
+    static final int DRAG = 1;
+    static final int ZOOM = 2;
+    int mode = NONE;
+    PointF start = new PointF();
+    PointF mid = new PointF();
+    float oldDist = 1f;
     ImageView iv;
     static public Bitmap bitmap;
     @Override
@@ -19,13 +34,67 @@ public class Popupimage extends Activity {
 
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
-
         int width = dm.widthPixels;
         int height = dm.heightPixels;
-
         getWindow().setLayout((int)(width*0.7),(int)(height*0.7));
-
         iv.setImageBitmap(bitmap);
+        iv.setOnTouchListener(this);
 
     }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        ImageView view = (ImageView) v;
+        view.setScaleType(ImageView.ScaleType.MATRIX);
+        float scale;
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:   // first finger down only
+                savedMatrix.set(matrix);
+                start.set(event.getX(), event.getY());
+                mode = DRAG;
+                break;
+            case MotionEvent.ACTION_UP: // first finger lifted
+            case MotionEvent.ACTION_POINTER_UP: // second finger lifted
+                mode = NONE;
+                break;
+            case MotionEvent.ACTION_POINTER_DOWN: // first and second finger down
+                oldDist = spacing(event);
+                if (oldDist > 5f) {
+                    savedMatrix.set(matrix);
+                    midPoint(mid, event);
+                    mode = ZOOM;
+                }
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (mode == DRAG) {
+                    matrix.set(savedMatrix);
+                    matrix.postTranslate(event.getX() - start.x, event.getY() - start.y);
+                } else if (mode == ZOOM) {
+                    // pinch zooming
+                    float newDist = spacing(event);
+                    if (newDist > 5f) {
+                        matrix.set(savedMatrix);
+                        scale = newDist / oldDist;
+                        matrix.postScale(scale, scale, mid.x, mid.y);
+                    }
+                }
+                break;
+        }
+        view.setImageMatrix(matrix);
+        return true;
+    }
+    private float spacing(MotionEvent event)
+    {
+        float x = event.getX(0) - event.getX(1);
+        float y = event.getY(0) - event.getY(1);
+        return (float) Math.sqrt(x * x + y * y);
+    }
+
+    private void midPoint(PointF point, MotionEvent event)
+    {
+        float x = event.getX(0) + event.getX(1);
+        float y = event.getY(0) + event.getY(1);
+        point.set(x / 2, y / 2);
+    }
+
 }
