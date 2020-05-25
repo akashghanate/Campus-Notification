d0057b6bacd31771ba6e746b0caa04eafc5b71a0
aku98 <akashghanate98@gmail.com>
2020-05-25 21:02:01 +0530

    new branch

--- app/src/main/java/com/example/abhinandan/onlinedb/MainActivity.java
+++ app/src/main/java/com/example/abhinandan/onlinedb/MainActivity.java
@@ -1,10 +1,9 @@
 package com.example.abhinandan.onlinedb;
 
-import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
